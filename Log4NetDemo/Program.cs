3a7cdabf61fdf5bd1d8656f298e884b5bcb41b01
César Rodríguez <cesar.rrguez@gmail.com>
2020-05-31 17:24:23 +0200

    Add ConsoleAppender with thread info

--- Log4NetDemo/Program.cs
+++ Log4NetDemo/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using log4net;
 
 namespace Log4NetDemo
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
+
+        private static void Main()
         {
+            Console.WriteLine("Welcome to Log4NetDemo");
+            Console.WriteLine("----------------------\n");
+
+            Log.Info("This is an info message");
+            Log.Warn("This is a warning message");
+            Log.Error("This is an error message");
+            Log.Fatal("This is a fatal message");
+
+            Console.WriteLine("\nPress any key to exist");
+            Console.ReadKey();
         }
     }
 }
