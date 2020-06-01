b09513339a1b5a08d9464ad1760cd624ac4585a1
César Rodríguez <cesar.rrguez@gmail.com>
2020-06-01 09:13:49 +0200

    Add ConsoleAppender with property

---
+++ Log4NetDemo/Simulation.cs
@@ -0,0 +1,18 @@
+﻿using log4net;
+
+namespace Log4NetDemo
+{
+    public class Simulation
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Simulation));
+
+        public void RunSimulation()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                GlobalContext.Properties["Counter"] = i;
+                Log.Debug("This is a new iteration");
+            }
+        }
+    }
+}
